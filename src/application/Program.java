4e0435937d13a7d3d2639cf98ad7557234f9cf9a
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-13 21:25:59 -0300

    Criado a entidade

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        // apenas teste
+        Department department = new Department(1, "Books");
+        System.out.println(department);
+
+
+    }
+}
---
+++ src/entities/Department.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Department implements Serializable {
+
+    private Integer Id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(Id, that.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
