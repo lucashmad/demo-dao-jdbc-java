ada679147e1cf7a94a691703f71efc57c64d91d5
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-14 21:53:19 -0300

    Criado a referencia do banco

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(21,"Lucas", "lucas@gmail.com", new Date(), 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
