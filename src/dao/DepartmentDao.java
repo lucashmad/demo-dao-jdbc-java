6fb91d47c32025e3e32c1e6f80ae5aa4d2671498
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-14 21:45:06 -0300

    criado os metodos do banco

---
+++ src/dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department department);
+    void update(Department department);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+
+}
