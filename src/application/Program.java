69ca2fc09a71f54c213cfa0f9e36eec13c3720b1
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-14 21:37:18 -0300

    criado entidade seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         // apenas teste
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(21,"Lucas", "lucas@gmail.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
+
 
 
     }
