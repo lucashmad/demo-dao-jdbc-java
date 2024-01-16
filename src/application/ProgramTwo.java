06f285e5b5cb6cda0660227f0c23fdce3afe56ca
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-16 18:14:35 -0300

    Implementado delete department

--- src/application/ProgramTwo.java
+++ src/application/ProgramTwo.java
@@ -4,12 +4,14 @@ import dao.DaoFactory;
 import dao.DepartmentDao;
 import entities.Department;
 
+import java.util.Scanner;
+
 
 public class ProgramTwo {
 
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
 
         DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
 
@@ -18,6 +20,11 @@ public class ProgramTwo {
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! New id: " + newDepartment.getId());
 
+        System.out.println("==== TEST 1: DELETE ====");
+        System.out.println("Enter id from delete: ");
+        int id = scanner.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Deleted completed! ");
 
 
 
--- src/dao/impl/DepartmentDaoJDBC.java
+++ src/dao/impl/DepartmentDaoJDBC.java
@@ -51,11 +51,47 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department department) {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE department "
+                    + "SET Name = ? "
+                    + "WHERE id = ? "
+            );
+            preparedStatement.setString(1, department.getName());
+            preparedStatement.setInt(2, department.getId());
+            preparedStatement.executeUpdate();
+
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(preparedStatement);
+        }
 
     }
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+            preparedStatement.setInt(1, id);
+
+            int rows = preparedStatement.executeUpdate();
+
+            if (rows == 0){
+                throw new DbException("Id not exist!");
+            }
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+           DB.closeStatement(preparedStatement);
+        }
 
     }
 
