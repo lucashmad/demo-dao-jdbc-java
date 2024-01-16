ecca75c9d2421db858840af0db67a2bfb8e9ed1f
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-01-16 13:24:09 -0300

    Implementado metodo update

--- src/application/Program.java
+++ src/application/Program.java
@@ -44,5 +44,10 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("=== Test 5: Seller Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Juliana Vitoria");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 }
